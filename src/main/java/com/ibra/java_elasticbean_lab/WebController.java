fe22824185d375f61fd2ba3a1436d60fd05190c4
Surface Book <iasafianu@st.ug.edu.gh>
2025-08-31 23:28:59 +0000

    trigger

--- src/main/java/com/ibra/java_elasticbean_lab/WebController.java
+++ src/main/java/com/ibra/java_elasticbean_lab/WebController.java
@@ -17,7 +17,7 @@ public class WebController {
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>AWS Elastic Beanstalk - Spring Boot</title>
+                <title>AWS Elastic Beanstalk - Spring Boot triggered</title>
                 <style>
                     * { margin: 0; padding: 0; box-sizing: border-box; }
                     body { 
